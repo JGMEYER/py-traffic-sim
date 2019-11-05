83af07f43f070d1798aa504c3ee68eee5a578a5d
JGMEYER <meyer.justian@gmail.com>
2019-11-05 03:20:39 -0500

    Rewrite Tile rendering logic to support variable tile and road sizes

--- src/road/graphics.py
+++ src/road/graphics.py
@@ -5,33 +5,207 @@ import pygame
 from .constants import TileType
 
 # WARNING: for now, do not modify - tile polygons will not scale to dimensions
-TILE_WIDTH = 30
-TILE_HEIGHT = 30
+TILE_WIDTH = 64
+TILE_HEIGHT = 64
+ROAD_WIDTH = 30
 
-TILE_POLYGONS = {
+# Constraints
+assert TILE_WIDTH % 2 == 0
+assert TILE_HEIGHT % 2 == 0
+assert ROAD_WIDTH % 2 == 0
+
+
+def tile_poly(up=False, right=False, down=False, left=False):
+    """Construct polygon based on road connections.
+
+    The calculations of these point locations depends on the width and height
+    of our tiles as well as the width of our roads.
+
+    To do this we step through each road direction clockwise, starting with UP.
+    We then add a certain number of points to the polygon for each direction
+    depending on whether the tile contains a road for the specified direction.
+
+    Take for example an UP_DOWN_LEFT road tile:
+
+        ┌───┬───┬───┬───┐
+        │   │ * │ * │   │
+        ├───┼───┼───┼───┤
+        │ * │ * │ * │   │
+        ├───┼───┼───┼───┤
+        │ * │ * │ * │   │
+        ├───┼───┼───┼───┤
+        │   │ * │ * │   │
+        └───┴───┴───┴───┘
+
+    Each cell in the grid represents a point along the road tile, not a pixel
+    on the final rendered tile. Though, if it's helpful, you can also imagine
+    these represent a 4x4 pixel tile with a road width of 2.
+
+    We determine these points through the following steps:
+
+    STEP 1: UP
+
+        ┌───┬───┬───┬───┐
+        │   │ 0 │ 1 │   │
+        ├───┼───┼───┼───┤
+        │   │ x │ 2 │   │
+        ├───┼───┼───┼───┤
+        │   │   │   │   │
+        ├───┼───┼───┼───┤
+        │   │   │   │   │
+        └───┴───┴───┴───┘
+
+        An UP_DOWN_LEFT tile has a road going in the UP direction, so we add
+        the 3 numbered points shown.
+
+        As we step through each direction, we don't care about the "clockwise"-
+        most point as we traverse the perimeter of the road. These will be
+        filled in either the previous or future steps. The point marked with an
+        'x', for example, will later be added in the LEFT step.
+
+    STEP 2: RIGHT
+
+        ┌───┬───┬───┬───┐
+        │   │   │   │   │
+        ├───┼───┼───┼───┤
+        │   │   │ x │   │
+        ├───┼───┼───┼───┤
+        │   │   │ 3 │   │
+        ├───┼───┼───┼───┤
+        │   │   │   │   │
+        └───┴───┴───┴───┘
+
+        An UP_DOWN_LEFT tile does NOT have a road going in the RIGHT direction,
+        so we add the 1 point shown.
+
+        The point marked with an 'x' has already been added in our previous UP
+        step and therefore no longer needs to be added to complete our polygon.
+
+    STEP 3: DOWN
+
+        ┌───┬───┬───┬───┐
+        │   │   │   │   │
+        ├───┼───┼───┼───┤
+        │   │   │   │   │
+        ├───┼───┼───┼───┤
+        │   │ 6 │   │   │
+        ├───┼───┼───┼───┤
+        │   │ 5 │ 4 │   │
+        └───┴───┴───┴───┘
+
+        An UP_DOWN_LEFT tile has a road going in the DOWN direction, so we add
+        the 3 points shown.
+
+    STEP 4: LEFT
+
+        ┌───┬───┬───┬───┐
+        │   │   │   │   │
+        ├───┼───┼───┼───┤
+        │ 8 │ 9 │   │   │
+        ├───┼───┼───┼───┤
+        │ 7 │   │   │   │
+        ├───┼───┼───┼───┤
+        │   │   │   │   │
+        └───┴───┴───┴───┘
+
+        An UP_DOWN_LEFT tile has a road going in the LEFT direction, so we add
+        the 3 points above.
+
+        We now have a set of points that matches our first illustration.
+
+    In summary, we add either 1 or 3 points depending on whether the road tile
+    has a road segment going in a particular direction. We only include points
+    necessary to complete to polygon and to avoid redundancy.
+
+    IMPORTANT NOTE:
+
+    This strategy creates redundant points on fully straight edges. e.g. an
+    UP_DOWN_LEFT tile really only needs the following points to render:
+
+        ┌───┬───┬───┬───┐
+        │   │ * │ * │   │
+        ├───┼───┼───┼───┤
+        │ * │ * │   │   │
+        ├───┼───┼───┼───┤
+        │ * │ * │   │   │
+        ├───┼───┼───┼───┤
+        │   │ * │ * │   │
+        └───┴───┴───┴───┘
+
+    However, this strategy was done to keep the logic simple and to iterate
+    quickly on adding tiles with dynamic sizes. If needed, this can be
+    optimized later.
+    """
+    points = []
+
+    # Shorten variable names
+    tw = TILE_WIDTH
+    th = TILE_HEIGHT
+    rw = ROAD_WIDTH
+
+    if up:
+        points.extend(
+            [(tw//2-(rw//2-1), 0),
+             ((tw//2+1)+(rw//2-1), 0),
+             ((tw//2+1)+(rw//2-1), th//2-(rw//2-1))])
+    else:
+        points.extend(
+            [((tw//2+1)+(rw//2-1), th//2-(rw//2-1))])
+
+    if right:
+        points.extend(
+            [(tw-1, th//2-(rw//2-1)),
+             (tw-1, (th//2+1)+(rw//2-1)),
+             ((tw//2+1)+(rw//2-1), (th//2+1)+(rw//2-1))])
+    else:
+        points.extend(
+            [((tw//2+1)+(rw//2-1), (th//2+1)+(rw//2-1))])
+
+    if down:
+        points.extend(
+            [((tw//2+1)+(rw//2-1), th-1),
+             (tw//2-(rw//2-1), th-1),
+             (tw//2-(rw//2-1), (th//2+1)+(rw//2-1))])
+    else:
+        points.extend(
+            [(tw//2-(rw//2-1), (th//2+1)+(rw//2-1))])
+
+    if left:
+        points.extend(
+            [(0, (th//2+1)+(rw//2-1)),
+             (0, th//2-(rw//2-1)),
+             (tw//2-(rw//2-1), th//2-(rw//2-1))])
+    else:
+        points.extend(
+            [(tw//2-(rw//2-1), th//2-(rw//2-1))])
+
+    return points
+
+
+TILE_POLYS = {
     # no tile
     TileType.EMPTY: [],
     # no neighbors
-    TileType.ALONE: [(10, 10), (20, 10), (20, 20), (10, 20)],
+    TileType.ALONE: tile_poly(),
     # one neighbor
-    TileType.UP: [(10, 0), (20, 0), (20, 20), (10, 20)],
-    TileType.RIGHT: [(10, 10), (30, 10), (30, 20), (10, 20)],
-    TileType.DOWN: [(10, 10), (20, 10), (20, 30), (10, 30)],
-    TileType.LEFT: [(0, 10), (20, 10), (20, 20), (0, 20)],
+    TileType.UP: tile_poly(up=True),
+    TileType.RIGHT: tile_poly(right=True),
+    TileType.DOWN: tile_poly(down=True),
+    TileType.LEFT: tile_poly(left=True),
     # two neighbors
-    TileType.UP_RIGHT: [(10, 0), (20, 0), (20, 10), (30, 10), (30, 20), (10, 20), (10, 0)],
-    TileType.RIGHT_DOWN: [(10, 10), (30, 10), (30, 20), (20, 20), (20, 30), (10, 30), (10, 10)],
-    TileType.DOWN_LEFT: [(0, 10), (20, 10), (20, 30), (10, 30), (10, 20), (0, 20)],
-    TileType.UP_LEFT: [(10, 0), (20, 0), (20, 20), (0, 20), (0, 10), (10, 10)],
-    TileType.UP_DOWN: [(10, 0), (20, 0), (20, 30), (10, 30)],
-    TileType.RIGHT_LEFT: [(0, 10), (30, 10), (30, 20), (0, 20)],
+    TileType.UP_RIGHT: tile_poly(up=True, right=True),
+    TileType.RIGHT_DOWN: tile_poly(right=True, down=True),
+    TileType.DOWN_LEFT: tile_poly(down=True, left=True),
+    TileType.UP_LEFT: tile_poly(up=True, left=True),
+    TileType.UP_DOWN: tile_poly(up=True, down=True),
+    TileType.RIGHT_LEFT: tile_poly(right=True, left=True),
     # three neighbors
-    TileType.UP_RIGHT_DOWN: [(10, 0), (20, 0), (20, 10), (30, 10), (30, 20), (20, 20), (20, 30), (10, 30)],
-    TileType.RIGHT_DOWN_LEFT: [(0, 10), (30, 10), (30, 20), (20, 20), (20, 30), (10, 30), (10, 20), (0, 20)],
-    TileType.UP_DOWN_LEFT: [(10, 0), (20, 0), (20, 30), (10, 30), (10, 20), (0, 20), (0, 10), (10, 10)],
-    TileType.UP_RIGHT_LEFT: [(10, 0), (20, 0), (20, 10), (30, 10), (30, 20), (0, 20), (0, 10), (10, 10)],
+    TileType.UP_RIGHT_DOWN: tile_poly(up=True, right=True, down=True),
+    TileType.RIGHT_DOWN_LEFT: tile_poly(right=True, down=True, left=True),
+    TileType.UP_DOWN_LEFT: tile_poly(up=True, down=True, left=True),
+    TileType.UP_RIGHT_LEFT: tile_poly(up=True, right=True, left=True),
     # four neighbors
-    TileType.ALL: [(10, 0), (20, 0), (20, 10), (30, 10), (30, 20), (20, 20), (20, 30), (10, 30), (10, 20), (0, 20), (0, 10), (10, 10)],
+    TileType.ALL: tile_poly(up=True, right=True, down=True, left=True)
 }
 
 
@@ -47,7 +221,7 @@ def render_road_network(window, network, include_travel_paths=True):
 
 def render_tile(window, r, c, road_type):
     """Draw single road tile to window"""
-    points = TILE_POLYGONS[road_type]
+    points = TILE_POLYS[road_type]
     if not points:
         return
     translated_points = [(x + TILE_WIDTH * c, y + TILE_HEIGHT * r)
