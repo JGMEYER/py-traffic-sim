15d671702f77ef7ff6b2ae7d5a7b68465fb273e6
JGMEYER <meyer.justian@gmail.com>
2019-11-05 01:08:29 -0500

    Cleanup imports in game.py

--- src/game.py
+++ src/game.py
@@ -2,16 +2,15 @@ import pygame
 import sys
 
 import input
-from road import grid as road_grid  # TODO fix imports / names
-from road import graphics as road_graphics  # TODO fix imports / names
-from road import network as road_network # TODO fix imports / names
+from road import graphics as road_gfx
+from road.network import RoadNetwork
 
 GRID_WIDTH = 30
 GRID_HEIGHT = 25
 
 # Create width and height constants
-WINDOW_WIDTH = road_graphics.TILE_WIDTH * GRID_WIDTH
-WINDOW_HEIGHT = road_graphics.TILE_HEIGHT * GRID_HEIGHT
+WINDOW_WIDTH = road_gfx.TILE_WIDTH * GRID_WIDTH
+WINDOW_HEIGHT = road_gfx.TILE_HEIGHT * GRID_HEIGHT
 
 
 def init():
@@ -43,7 +42,7 @@ def game_loop(window):
     """Game loop"""
 
     # Create road network
-    network = road_network.RoadNetwork(GRID_WIDTH, GRID_HEIGHT)
+    network = RoadNetwork(GRID_WIDTH, GRID_HEIGHT)
 
     while 1:
         # Process user and window inputs
@@ -57,11 +56,11 @@ def game_loop(window):
         display_tile_cursor(window)
 
         # Render road network
-        road_graphics.render_road_network(window, network)
+        road_gfx.render_road_network(window, network)
 
         # Render path to last placed tile
         if last_path:
-            road_graphics.render_path(window, last_path)
+            road_gfx.render_path(window, last_path)
 
         # Update our display
         pygame.display.update()
@@ -81,8 +80,8 @@ def process_input(window, network):
 
 def process_mouse_button_down(window, network):
     """Place new tile on grid"""
-    r, c = input.mouse_coord_to_grid_coord(road_graphics.TILE_WIDTH,
-                                           road_graphics.TILE_HEIGHT)
+    r, c = input.mouse_coord_to_grid_coord(road_gfx.TILE_WIDTH,
+                                           road_gfx.TILE_HEIGHT)
     road_added = network.add_road((r, c))
     if road_added:
         global last_path
@@ -91,12 +90,12 @@ def process_mouse_button_down(window, network):
 
 def display_tile_cursor(window):
     """Highlights tile underneath mouse"""
-    r, c = input.mouse_coord_to_grid_coord(road_graphics.TILE_WIDTH,
-                                           road_graphics.TILE_HEIGHT)
-    x = c * road_graphics.TILE_WIDTH
-    y = r * road_graphics.TILE_HEIGHT
-    rect = pygame.Rect(x, y, road_graphics.TILE_WIDTH,
-                       road_graphics.TILE_HEIGHT)
+    r, c = input.mouse_coord_to_grid_coord(road_gfx.TILE_WIDTH,
+                                           road_gfx.TILE_HEIGHT)
+    x = c * road_gfx.TILE_WIDTH
+    y = r * road_gfx.TILE_HEIGHT
+    rect = pygame.Rect(x, y, road_gfx.TILE_WIDTH,
+                       road_gfx.TILE_HEIGHT)
     pygame.draw.rect(window, (0, 255, 255), rect)
 
 
